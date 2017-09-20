2e46dc996b83f2da3e39d0e361ddb00ae588a433
Sridhar Nelloru <snelloru@gmail.com>
2017-09-19 20:39:24 -0700

    Adding myuserdetails.sh

---
+++ snelloru/myuserdetails.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Write your scrpt here
+#username: snelloru
+#groups: devops
+#My Hostname is **** and my domain name is ****
+#Current Date: yyyy/mm/dd
+#Current Time: HH:MM:SS
+#Day of Today: Tuesday
+#My Time zone: PST/PDT/CST
+#Current logged in users in server:
+#Total directories in my home dir: xx
+#Total files in my home dir : xxx
+
