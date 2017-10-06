4df06c468e6c2b17b24dc1c7d6db34592624c807
nikithateki93 <nikitha.teki@gmail.com>
2017-10-06 10:50:39 -0700

    Adding check-disk-space.sh

--- nteki/check-disk-space.sh
+++ nteki/check-disk-space.sh
@@ -4,14 +4,18 @@ echo "              "
 echo "              "
 echo "              "
 echo "              "
-echo "Total disk space:`df -h|head -2|tail -1|cut -d ' ' -f8`" 
+echo "Total disk space:`df -h|head -2|tail -1|cut -d ' ' -f8`"
 echo "Available Disk:`df -h|head -2|tail -1|cut -d ' ' -f14`"
 var1=`df -h|head -2|tail -1|cut -d ' ' -f14`
-if [ $var1 > $1 ]; then
-echo "              "
-echo "************************"
-echo "INFO:Dev1.devopscave.com has disk space $var1 which is > $1"
-echo "************************"
-exit 1
-fi   
+if [[ "$var1" > "$1" ]]; then
+        echo "              "
+        echo "************************"
+        echo "INFO:Dev1.devopscave.com has disk space $var1 which is > $1"
+        echo "************************"
+else
+        echo "              "
+        echo "************************"
+        echo "ALERT:Dev1.devopscave.com has disk space $var1 which is < $1"
+        echo "************************"
+fi
 
